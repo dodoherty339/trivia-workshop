22d9fecddf7c6f0bdb5f747e4c89aab8580b5d5d
Steven Schulteis <schult@gmail.com>
2016-10-26 11:14:52 -0500

    Remove question creation method.

--- java/src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
+++ java/src/main/java/com/adaptionsoft/games/uglytrivia/Game.java
@@ -17,18 +17,14 @@ public class Game {
     private int currentPlayer = 0;
     private boolean isGettingOutOfPenaltyBox;
     
-    public  Game(){
+    public Game(){
     	for (int i = 0; i < 50; i++) {
 			popQuestions.addLast("Pop Question " + i);
-			scienceQuestions.addLast(("Science Question " + i));
-			sportsQuestions.addLast(("Sports Question " + i));
-			rockQuestions.addLast(createRockQuestion(i));
+			scienceQuestions.addLast("Science Question " + i);
+			sportsQuestions.addLast("Sports Question " + i);
+			rockQuestions.addLast("Rock Question " + i);
     	}
     }
-
-	private String createRockQuestion(int index){
-		return "Rock Question " + index;
-	}
 
 	public boolean add(String playerName) {
 	    players.add(playerName);
